a3d12595eefb427e356b0d3c8838904d9e971757
Benjamin Geyer <brgeyer49@gmail.com>
2018-09-13 12:04:06 -0400

    final commit

--- main.py
+++ main.py
@@ -43,27 +43,74 @@ def highest_domestic_gross(movies):
     return maxGrossMovie
 
 
-# consultants algo
+# consultants algorithm ans stats
 def outside_consultant_predicted_revenue(budget):
     return (1.5*budget + 10)
 
+def error_for_consultant_model(movie):
+    return movie['domgross_2013$'] - outside_consultant_predicted_revenue(movie['budget_2013$'])
 
-#DATA
-#set movie list to scaled down
+def rss_consultant(movies):
+    return sum(list(map(lambda el: round(error_for_consultant_model(el)**2,2),movies)))
+
+# our model
+def revenue_with_year(budget, year):
+    return (budget*1.1)+(year-1965)*1.5
+
+def error_revenue_with_year(movie):
+    return movie['domgross_2013$']-revenue_with_year(movie['budget_2013$'], movie['year'])
+
+def rss_revenue_with_year(movies):
+    return round(sum(list(map(lambda el: error_revenue_with_year(el)**2,movies))),2)
+
+initial_regression_line = {'b': 0.5, 'm': 1.79}
+def expected_revenue_per_budget(budget):
+    return 1.79*budget + 0.5
+
+def regression_revenue_error(m, b, movie):
+    return round(movie['domgross_2013$']-(m*movie['budget']+b),2)
+
+def b_gradient(m, b, movies):
+    resultSum = 0
+    for movie in movies:
+        resultSum += regression_revenue_error(m, b, movie)
+    return -2 * resultSum
+
+def generate_steps(m, b, number_of_steps, movies, learning_rate):
+    iterations = []
+    for i in range(number_of_steps):
+        iteration = step_gradient(b,m,movies,learning_rate)
+        b = iteration['b']
+        m = iteration['m']
+        iterations.append(iteration)
+    return iterations
+
+
+# DATA
+# set movie list to scaled down
 scaled_movies = scale_down_movies(parsed_movies)
 budgets = list(map(lambda movie: movie['budget_2013$'], scaled_movies))
 domestic_revenues = list(map(lambda movie: movie['domgross_2013$'], scaled_movies))
 titles = list(map(lambda movie: movie['title'], scaled_movies))
 
 consultant_estimated_revenues = list(map(lambda budget: outside_consultant_predicted_revenue(budget),budgets))
+internal_consultant_estimated_revenues = list(map(lambda movie: revenue_with_year(movie['budget_2013$'], movie['year']),scaled_movies))
+#internal_consultant_estimated_trace = trace_values(budgets, internal_consultant_estimated_revenues, mode='markers', name = 'internal consultant estimate')
 
+# PLOTLY outputs in seperate html files
+trace1 = go.Scatter(x=budgets, y=domestic_revenues, text = titles, mode="markers", name="revenues")
+trace2 = go.Scatter(x=budgets, y=consultant_estimated_revenues, text = titles, mode="lines")
+trace3 = go.Scatter(x=budgets, y=internal_consultant_estimated_revenues, text = titles, mode="markers")
 
-
-
-#PLOTLY outputs in seperate html files
-trace1 = go.Scatter(x=budgets, y=domestic_revenues, text = titles, mode="markers")
-trace2 = go.Scatter(x=budgets, y=consultant_estimated_revenues, text = titles, mode="markers")
-data=go.Data([trace1, trace2])
-layout=go.Layout(title="Movies!", xaxis={'title':'Budget'}, yaxis={'title':'Revenue'})
+data=go.Data([trace1, trace2, trace3])
+layout=go.Layout(title="Movies", xaxis={'title':'Budget', 'range':[0,300]}, yaxis={'title':'Revenue','range':[0,1000]}, )
 figure=go.Figure(data=data, layout=layout)
 py.offline.plot(figure, filename='file.html')
+
+# New Model
+years = list(map(lambda movie: movie['year'],movies))
+years_and_revenues = go.Scatter(x=years, y=domestic_revenues, text = titles, mode="markers")
+data1 = go.Data([years_and_revenues])
+layout1 = go.Layout(title="New Model")
+figure1 = go.Figure(data=data1, layout=layout1)
+#py.offline.plot(figure1, filename='file1.html')
